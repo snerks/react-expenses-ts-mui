40601f87b21102e5049f569bc018007edff5554f
snerks <martin.github@outlook.com>
2020-03-31 18:45:40 +0100

    Updates

---
+++ precache-manifest.daee526c928ffe0020e4ff6156608204.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "352f69f309f43bc2c7e7202add380e2f",
+    "url": "/react-expenses-ts-mui/index.html"
+  },
+  {
+    "revision": "488f75af887719622552",
+    "url": "/react-expenses-ts-mui/static/js/2.2d140bb5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-expenses-ts-mui/static/js/2.2d140bb5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a6a4f038c94462ccba6",
+    "url": "/react-expenses-ts-mui/static/js/main.967ee5a3.chunk.js"
+  },
+  {
+    "revision": "3aee779ab4f63b1d034b",
+    "url": "/react-expenses-ts-mui/static/js/runtime-main.73165ccc.js"
+  }
+]);
